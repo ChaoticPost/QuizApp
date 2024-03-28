ca6992cb9904119f2d5c5e43d70ff72cb54b390a
Daria Chugunova <dariachugunova2003@gmail.com>
2024-03-28 17:11:37 +0300

    add: Updated the color palettes in the file Color.kt

--- app/src/main/java/com/daria/example/quizapp/ui/theme/Color.kt
+++ app/src/main/java/com/daria/example/quizapp/ui/theme/Color.kt
@@ -2,6 +2,28 @@ package com.daria.example.quizapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val White = Color(0xFFFFFFFF)
+
+//Shade of grey
+val Grey = Color(0xFFD6D9DD)
+val Dark_grey = Color(0xFF929292)
+val Grey_description = Color(0xFF747474)
+
+//Shade of green
+val Faint_light_green = Color(0xFFEBF3EC)
+val Faint_green = Color(0xFF3C403D)
+val Faint_green_night = Color(0xFF3A4E3E)
+val Green = Color(0xFF2F7F41)
+val Dark_green = Color(0xFF38452D)
+val Green_dark_theme = Color(0xFF1C2317)
+val Light_green_dark_theme = Color(0xFF38452D)
+val Button_dark_theme = Color(0xFF213A0F)
+
+//Shade of black
+val Graphite = Color(0xFF323137)
+
+
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
